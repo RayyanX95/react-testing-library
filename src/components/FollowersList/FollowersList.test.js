e15584d74b60311593d90b58f2a527d373d11b31
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2023-01-21 10:41:09 +0200

    Add tests to followersList component

---
+++ src/components/FollowersList/FollowersList.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "./FollowersList";
+
+const MockFollowersList = () => (
+  <BrowserRouter>
+    <FollowersList />
+  </BrowserRouter>
+);
+
+test("should render a followersList item", async () => {
+  render(<MockFollowersList />);
+  const followersItem = await screen.findByTestId("follower-item-0");
+
+  expect(followersItem).toBeInTheDocument();
+});
+
+test("should render a multiple followersList items", async () => {
+  render(<MockFollowersList />);
+  const followersItems = await screen.findAllByTestId(/follower-item/i);
+
+  expect(followersItems.length).toBe(5);
+});
